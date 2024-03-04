1b7ecff96089edc542b65419e8b70d8492829663
Salomon Romano <salo_romano@yahoo.com>
2024-03-04 16:48:39 -0600

    Test

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,28 +1,26 @@
 import getDomain from '@/app/lib/getDomain'
 
 async function getData() {
-  // const domain = getDomain()
-  // const endpoint = `${domain}/api/posts`
-  // const res = await fetch(endpoint, { next: { revalidate: 10 } })
+  const domain = getDomain()
+  const endpoint = `${domain}/api/posts`
+  const res = await fetch(endpoint, { next: { revalidate: 10 } })
 
-  // console.log(`endpoint`, endpoint)
+  console.log(`status`, res.status)
 
-  // if (!res.ok) {
-  //   throw new Error('Failed to fetch data')
-  // }
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
 
-  // if (res.headers.get('content-type') !== 'application/json') {
-  //   return { items: [] }
-  // }
+  if (res.headers.get('content-type') !== 'application/json') {
+    return { items: [] }
+  }
 
-  // return res.json()
-  return { items: [] }
+  return res.json()
 }
 
 export default async function BlogPage() {
   const data = await getData()
   const items = data && data.items ? [...data.items] : []
-  console.log(items)
   return (
     <main>
       <h1>Hello World</h1>
